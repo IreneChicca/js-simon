2884ee85bb5d1fd2a4b6f0a5a8bd463120fd9b1a
IreneChicca <irene.chicca@outlook.com>
2023-08-23 16:03:58 +0200

    ADD counter function

--- main.js
+++ main.js
@@ -8,17 +8,24 @@ const todayDate = new Date();
 
 const tomorrowDate = new Date("2023-08-24 9:30");
 
-const timeDiff = Math.floor(tomorrowDate-todayDate).valueOf()
+let timeDiff = Math.floor(tomorrowDate-todayDate).valueOf()
 console.log(timeDiff)
 
-let secDiff = parseInt((timeDiff/1000)%60)
-let minDiff = parseInt((timeDiff/(1000*60)%60))
-let hourDiff = parseInt((timeDiff/(1000*60*60))%24)
-
-console.log(hourDiff+":"+minDiff+":"+secDiff)
 
 
-hour.innerHTML = hourDiff;
-minutes.innerHTML = minDiff;
-sec.innerHTML = secDiff;
 
+
+setInterval(function(){
+
+    timeDiff = timeDiff - 1000;
+    let secDiff = parseInt((timeDiff/1000)%60)
+    let minDiff = parseInt((timeDiff/(1000*60)%60))
+    let hourDiff = parseInt((timeDiff/(1000*60*60))%24)
+
+    hour.innerHTML = hourDiff;
+    mins.innerHTML = minDiff;
+    sec.innerHTML = secDiff;
+    
+
+
+},1000);
