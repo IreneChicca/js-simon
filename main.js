a0f316f5f4dff234d4f7868ea98ec5086487dc7c
IreneChicca <irene.chicca@outlook.com>
2023-08-23 15:30:40 +0200

    Print on screen

--- main.js
+++ main.js
@@ -1,37 +1,12 @@
+// Recupero elementi di interesse
+
+const hour = document.getElementById('hours')
+const mins = document.getElementById('minutes')
+const sec = document.getElementById('seconds')
+
 const todayDate = new Date();
-console.log(todayDate)
-
-let todayMillis = todayDate.getMilliseconds()
-
-
-let todaySec = todayDate.getSeconds()
-
-
-let todayMin = todayDate.getMinutes()
-
-let todayHour = todayDate.getHours()
-
-console.log(todayHour+":"+todayMin+":"+todaySec+":"+todayMillis)
-
 
 const tomorrowDate = new Date("2023-08-24 9:30");
-console.log(tomorrowDate)
-
-let tomorrowMillis = tomorrowDate.getMilliseconds()
-
-
-let tomorrowSec = tomorrowDate.getSeconds()
-
-
-let tomorrowMin = tomorrowDate.getMinutes()
-
-let tomorrowHour = tomorrowDate.getHours()
-
-console.log(tomorrowHour+":"+tomorrowMin+":"+tomorrowSec+":"+tomorrowMillis)
-
-
-
-console.log((tomorrowHour-todayHour)+":"+(tomorrowMin-todayMin)+":"+(tomorrowSec-todaySec))
 
 const timeDiff = Math.floor(tomorrowDate-todayDate).valueOf()
 console.log(timeDiff)
@@ -43,4 +18,7 @@ let hourDiff = parseInt((timeDiff/(1000*60*60))%24)
 console.log(hourDiff+":"+minDiff+":"+secDiff)
 
 
+hour.innerHTML = hourDiff;
+minutes.innerHTML = minDiff;
+sec.innerHTML = secDiff;
 
